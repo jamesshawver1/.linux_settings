9ff4dd954982a61586a204a31960c3fe1933659b
James Shawver <jamesshawver1@gmail.com>
2015-09-16 23:22:13 -0600

    updating setup script to clone Vundle automatically

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ rm -f .profile .vimrc .bashrc
 ln -s $curr_dir/profile .profile
 ln -s $curr_dir/vimrc .vimrc
 ln -s $curr_dir/bashrc .bashrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo "remember to run :PluginInstall within vim"
